723f9c7f9094be6f22bd0bf0a8bb5d117c49ff10
demetreus420 <dima.net.dd@gmail.com>
2021-03-13 19:42:01 +0200

    /help added

--- index.js
+++ index.js
@@ -104,3 +104,14 @@ bot.onText(/history (.+)\/(.+)/, (message, matched) => {
         bot.sendMessage(message.chat.id, `Invalid second currency - ${matched[2]}`)
     }
 })
+
+bot.onText(/help/, (message) => {
+    bot.sendMessage(message.chat.id, `
+        /history <CURRENCY>/<CURRENCY>    - show a history of rates changes(not working)
+        /lst or /list       - show all rates
+        /exchange 10 <CURRENCY> to <CURRENCY>
+        /exchange 10 USD to CAD
+        or
+        /exchange 10$ to CAD - show exchange rate from USD to CAD 
+    `);
+})
